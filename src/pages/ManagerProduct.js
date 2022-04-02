161192b0b30a885c82cc9e1976dc6ee709e239a5
Steve <vkhanhqui@gmail.com>
2022-04-03 00:59:47 +0700

    Refactor Admin components

--- src/pages/ManagerProduct.js
+++ src/pages/ManagerProduct.js
@@ -1,39 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Menu, Table, Button } from "antd";
-import { FaUserMinus, FaUserPlus } from "react-icons/fa";
-import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import { useProductsContext } from "../context/products_context";
-import logo from "../assets/logo.png";
+import { Table, Button } from "antd";
 import { Link } from "react-router-dom";
 import getProducts from "../context/products_filter";
+import AdminHeader from "../components/admin_header";
+import AdminMenu from "../components/admin_menu";
 
 const ManagerProduct = () => {
-  const { closeSidebar } = useProductsContext();
-  const { SubMenu } = Menu;
   const [products, setProducts] = useState([]);
-
-  const rootSubmenuKeys = ["sub1", "sub2", "sub4"];
-
-  const [openKeys, setOpenKeys] = React.useState(["sub1"]);
-
-  const onOpenChange = (keys) => {
-    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
-
-  const Logout = () => {
-    localStorage.removeItem("token");
-    window.location.reload();
-  };
 
   useEffect(() => {
     getProducts().then((res) => setProducts(res));
@@ -68,95 +42,9 @@ const ManagerProduct = () => {
 
   return (
     <main>
-      <div
-        className="nav-header"
-        style={{ width: "100%", backgroundColor: "#8FBC8F", height: 150 }}
-      >
-        <Link to="/">
-          <img
-            src={logo}
-            alt="Green Life"
-            style={{
-              marginTop: -80,
-              height: 250,
-              marginLeft: 70,
-            }}
-          />
-        </Link>
-        {localStorage.getItem("token") ? (
-          <button
-            type="button"
-            className="auth-btn"
-            onClick={Logout}
-            style={{
-              "font-size": 27,
-              color: "black",
-              marginLeft: 770,
-              backgroundColor: "#8FBC8F",
-              border: "none",
-            }}
-          >
-            {localStorage.getItem("username")} <FaUserMinus />
-          </button>
-        ) : (
-          <Link
-            to="/login"
-            className="cart-btn"
-            onClick={closeSidebar}
-            style={{ "font-size": 27, color: "black", marginLeft: 770 }}
-          >
-            Login <FaUserPlus />
-          </Link>
-        )}
-      </div>
+      <AdminHeader />
       <Wrapper className="page section section-center">
-        <Menu
-          mode="inline"
-          // theme="dark"
-          openKeys={openKeys}
-          onOpenChange={onOpenChange}
-          style={{
-            width: 500,
-            height: 600,
-            marginLeft: -215,
-            marginTop: -80,
-            marginRight: -50,
-            marginBottom: -80,
-          }}
-          className="menu"
-        >
-          <Menu.Item key="sub1" icon={<MailOutlined />} title="Tổng quan">
-            Tổng quan
-          </Menu.Item>
-          <Menu.Item
-            key="sub2"
-            icon={<AppstoreOutlined />}
-            title="Quản Lý Đơn Hàng"
-          >
-            Quản Lý Đơn Hàng
-          </Menu.Item>
-          <Menu.Item
-            key="sub4"
-            icon={<SettingOutlined />}
-            title="Quản Lý Sản Phẩm"
-          >
-            Quản Lý Sản Phẩm
-          </Menu.Item>
-          <Menu.Item
-            key="sub5"
-            icon={<SettingOutlined />}
-            title="Quản Lý Loại Sản Phẩm"
-          >
-            Quản Lý Loại Sản Phẩm
-          </Menu.Item>
-          <Menu.Item
-            key="sub6"
-            icon={<SettingOutlined />}
-            title="Quản Lý Nhân Viên"
-          >
-            Quản Lý Nhân Viên
-          </Menu.Item>
-        </Menu>
+        <AdminMenu />
         <article>
           <div className="title" style={{ marginLeft: 50 }}>
             <h2
